5a71b422173ffe8f4bb5f7d9eb707339d053a476
Kaja Królikowska <krolikowska.kaja@gmail.com>
2022-12-12 13:49:35 +0100

    Added GitHub link.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">React Weather App</div>;
+  return (
+    <div className="App">
+      <h1>React Weather App</h1>
+      <p>
+        This app was coded by Kaja Królikowska, and is open-sourced on{" "}
+        <a href="https://github.com/kajakrolikowska/fp-react-weather-app">
+          GitHub
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
 
 export default App;
